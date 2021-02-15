64301ff506af6c731e17e87015e82fd8a5a78654
Megapovelitel <megapovelitel@yandex.com>
2021-02-15 19:16:45 +0700

    m

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ErrorPage() {
     return (
         <div style={{display: 'flex', justifyContent: 'center'}}>
-            <img height='700px'src='https://cdn.dribbble.com/users/1651691/screenshots/5336717/404_v2.png'></img>
+            <img height='750px'src='https://cdn.dribbble.com/users/1651691/screenshots/5336717/404_v2.png'></img>
         </div>
     )
 }
